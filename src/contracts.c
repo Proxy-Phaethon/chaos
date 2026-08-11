dcc893cbe3414ba430490e10de6c67c44492ab19
Zia Hawwa <rziahawwa@gmail.com>
2026-08-11 12:05:15 +0530

    Create contracts.c

---
+++ src/contracts.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "contracts.h"
+
+static void contract_print(const char *input)
+{
+    printf("%s\n", input);
+}
+
+static void contract_terminate(const char *input)
+{
+    (void)input;
+    exit(0);
+}
+
+Contract contracts[] =
+{
+    {"print", contract_print},
+    {"terminate", contract_terminate},
+    {NULL, NULL}
+};
+
+ContractFunction find_contract(const char *name)
+{
+    int i = 0;
+
+    while (contracts[i].name != NULL)
+    {
+        if (strcmp(name, contracts[i].name) == 0)
+        {
+            return contracts[i].function;
+        }
+
+        i++;
+    }
+
+    return NULL;
+}
