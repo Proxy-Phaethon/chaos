d3f10e89c965473dfa862f9ec9db71ee5b40a1da
Zia Hawwa <rziahawwa@gmail.com>
2026-08-30 13:39:20 +0530

    runtime edits

--- include/runtime.h
+++ include/runtime.h
@@ -5,77 +5,53 @@
 #include "runtime_state.h"
 
 typedef enum {
-    RUNTIME_FLOW_CONTINUE,
-    RUNTIME_FLOW_TERMINATE
-} RuntimeFlow;
+RUNTIME_SUCCESS,
+RUNTIME_ERROR,
+RUNTIME_TERMINATE
+} RuntimeResult;
 
 typedef struct {
-    RuntimeStateStore *states;
+RuntimeStateStore *states;
 } Runtime;
 
 Runtime *runtime_create(void);
 
 void runtime_free(
-    Runtime *runtime
+Runtime *runtime
 );
 
-int runtime_execute(
-    Runtime *runtime,
-    const ASTNode *program
+RuntimeResult runtime_execute(
+Runtime *runtime,
+const ASTNode *program
 );
 
-int runtime_execute_register(
-    Runtime *runtime,
-    const ASTNode *register_node
+RuntimeResult runtime_execute_register(
+Runtime *runtime,
+const ASTNode *register_node
 );
 
-int runtime_execute_logic(
-    Runtime *runtime,
-    const ASTNode *logic_node
+RuntimeResult runtime_execute_logic(
+Runtime *runtime,
+const ASTNode *logic_node
 );
 
-int runtime_evaluate_condition(
-    Runtime *runtime,
-    const ASTNode *condition
+RuntimeResult runtime_execute_data_structure_operation(
+Runtime *runtime,
+const ASTNode *operation
 );
 
-int runtime_execute_if(
-    Runtime *runtime,
-    const ASTNode *if_node
+RuntimeResult runtime_execute_constant(
+Runtime *runtime,
+const ASTNode *constant
 );
 
-int runtime_execute_else_if(
-    Runtime *runtime,
-    const ASTNode *else_if_node
-);
-
-int runtime_execute_else(
-    Runtime *runtime,
-    const ASTNode *else_node
-);
-
-int runtime_execute_terminate(
-    Runtime *runtime,
-    const ASTNode *terminate_node
-);
-
-int runtime_execute_data_structure_operation(
-    Runtime *runtime,
-    const ASTNode *operation
-);
-
-int runtime_execute_constant(
-    Runtime *runtime,
-    const ASTNode *constant
-);
-
-int runtime_execute_execute(
-    Runtime *runtime,
-    const ASTNode *execute_node
+RuntimeResult runtime_execute_execute(
+Runtime *runtime,
+const ASTNode *execute_node
 );
 
 void runtime_print_state(
-    const Runtime *runtime
+const Runtime *runtime
 );
 
 #endif
